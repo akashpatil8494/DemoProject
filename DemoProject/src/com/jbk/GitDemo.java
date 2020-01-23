d5545b497e7d1c0f3ec508a3d134ebc6572b01ff
akashpatil8494 <53179661+akashpatil8494@users.noreply.github.com>
2020-01-23 22:45:23 +0530

    Update GitDemo.java

--- DemoProject/src/com/jbk/GitDemo.java
+++ DemoProject/src/com/jbk/GitDemo.java
@@ -3,7 +3,8 @@ package com.jbk;
 public class GitDemo {
 	
 	public static void main(String[] args) {
-	Akash has return some code	
+	Akash has return some code
+		Changes made on master branch
 		
 		
 		
